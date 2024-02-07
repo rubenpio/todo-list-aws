0bd5da247b55fc3128e7bed6c4d6597a30de774b
rubenpio <152532760+rubenpio@users.noreply.github.com>
2024-02-07 18:39:41 +0100

    Update todoApiTest.py

--- test/integration/todoApiTest.py
+++ test/integration/todoApiTest.py
@@ -9,7 +9,7 @@ import pytest
 
 
 BASE_URL = os.environ.get("BASE_URL")
-BASE_URL = "https://${ServerlessRestApi}.execute-api.${AWS::Region}.amazonaws.com/Prod"
+#BASE_URL = "https://${ServerlessRestApi}.execute-api.${AWS::Region}.amazonaws.com/Prod"
 DEFAULT_TIMEOUT = 2  # in secs
 
 
